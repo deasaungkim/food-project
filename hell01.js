993a8b69592d7f4762690f2005085f94c98a8569
Suhyeon Gim <heypitcher@hanmail.net>
2022-09-13 14:24:11 +0900

    first commit

---
+++ hell01.js
@@ -0,0 +1,25 @@
+
+
+
+/*로그인 이벤트*/
+
+/*제공하는 서비스 박스 영역 
+
+*/
+
+
+/*function Person(name){
+  this.name=name;
+  this.introduce=function(){
+    return 'My name is '+this.name;
+  }
+}
+var p1=new Person('egoing');
+console.log(p1.introduce()+'<br />');
+
+var p2=new Person('leezche');
+console.log(p2.introduce()); */
+
+//짝수에 곱하기 4한후 합
+
+
