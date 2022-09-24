ecdb4a95cdd62a0fc7d14728089d9d58bd5100ff
deasaungkim <heypitcher@hanmail.net>
2022-09-24 21:37:06 +0900

    event check all(join)

--- hell01.js
+++ hell01.js
@@ -1,25 +1,15 @@
 
 
 
-/*로그인 이벤트*/
-
-/*제공하는 서비스 박스 영역 
-
-*/
-
-
-/*function Person(name){
-  this.name=name;
-  this.introduce=function(){
-    return 'My name is '+this.name;
-  }
-}
-var p1=new Person('egoing');
-console.log(p1.introduce()+'<br />');
-
-var p2=new Person('leezche');
-console.log(p2.introduce()); */
-
-//짝수에 곱하기 4한후 합
-
-
+function selectAll(selectAll) {
+	document.getElementsByName('test').forEach((checkbox) => {
+		checkbox.checked = selectAll.checked;
+	})
+};
+// 위의 코드와 같다
+// function selectAll(selectAll) {
+// 	const checkboxes = document.getElementName('');
+// 	checkboxes.forEach((checkbox) => {
+// 		checkbox.checked = selectAll.checked;
+// 	})
+// }
