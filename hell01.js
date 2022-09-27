262f34ab380d6877e4cbc9ea5a3216fb81424c8b
deasaungkim <heypitcher@hanmail.net>
2022-09-27 17:53:51 +0900

    BMI label(for)-input(name)

--- hell01.js
+++ hell01.js
@@ -21,27 +21,26 @@ function selectAll(selectAll) {
 /*몸무게의 값 const weight= document.getElementById('InputWeight').value */
 
 // var bt = document.getElementById("bmibt");
-// bt.addEventListener("submit", function BMIFunc() {
-// 	const height = document.getElementById('InputHeight').value
-// 	const weight = document.getElementById('InputWeight').value
-// 	var BMI = weight / (height ** 2);
-// }; console.log(BMI)
+// bt.addEventListener("submit", BMIFunc);
+// const height = document.getElementById('InputHeight').value
+// const weight = document.getElementById('InputWeight').value
+// var BMI = weight / (height ** 2);
+// bt.addEventListener('click', BMIFunc)
+function BMIFunc() {
+	const height = document.getElementById('InputHeight').value
+	const weight = document.getElementById('InputWeight').value
+	const result = document.getElementById('BMIResult')
+	var BMI = (weight / ((height ** 2) / 10000)).toFixed(2)
+	if (BMI <= 18.5) {
+		document.write('저체중').getElementById('BMIResult').value
+	} else if ((BMI > 18.5) && (BMI < 22.9)) {
+		var bmiclass = '정상'
+	} else if ((BMI > 23.0) && (BMI < 24.9)) {
+		var bmiclass = '과체중'
+	} else {
+		var bmiclass = '비만'
+	}
 
-	// function BMIFunc(BMISubmit) {
-	// 	const height = document.getElementById('InputHeight').value
-	// 	const weight = document.getElementById('InputWeight').value
-	// 	var BMI = weight / (height ** 2)
-	// 	console.log(BMI)
+	// document.getElementById('BMIResult').innerText = bmiclass;
 
-	// if (BMI <= 18.5) {
-	// 	bmiclass = '저체중'
-	// } else if ((BMI > 18.5) && (BMI < 22.9)) {
-	// 	bmiclass = '정상'
-	// } else if ((BMI > 23.0) && (BMI < 24.9)) {
-	// 	bmiclass = '과체중'
-	// } else {
-	// 	bmiclass = '비만'
-	// }
-
-	// document.getElementById('BMIResult').value = bmiclass;
-
+}
