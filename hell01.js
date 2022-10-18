f42c019f89f9b01e07c642fc1e567ba9c383cf9c
deasaungkim <heypitcher@hanmail.net>
2022-10-18 16:12:12 +0900

    LoginModal popup work->BMI not work

--- hell01.js
+++ hell01.js
@@ -84,37 +84,34 @@ function BMIFunc() {
 
 // db.dailyFood.inset_one({ 'morning': test1 })
 // return jsonity({ 'msg': "입력 완료" })
+window.onload = () => {
 
-const loremIpsum = document.getElementById("lorem-ipsum")
-fetch("https://baconipsum.com/api/?type=all-meat&paras=200&format=html")
-	.then(response => response.text())
-	.then(result => loremIpsum.innerHTML = result)
-
-const modal = document.getElementById("modal")
-function modalOn() {
-	modal.style.display = "flex"
-}
-function isModalOn() {
-	return modal.style.display === "flex"
-}
-function modalOff() {
-	modal.style.display = "none"
-}
-
-const btnModal = document.getElementById("btn_modal")
-btnModal.addEventListener("click", e => { modalOn() })
-
-const closeBtn = modal.querySelector(" .close-area")
-closeBtn.addEventListener("click", e => { modalOff() })
-
-modal.addEventListener("click", e => {
-	const evTarget = e.target
-	if (evTarget.classList.contains("modal-overlay")) {
-		modalOff()
+	const modal = document.getElementById("modal")
+	function modalOn() {
+		modal.style.display = "flex"
 	}
-})
-window.addEventListener("keyup", e => {
-	if (isModalOn() && e.key === "Escape") {
-		modalOff()
+	function isModalOn() {
+		return modal.style.display === "flex"
 	}
-})
+	function modalOff() {
+		modal.style.display = "none"
+	}
+
+	const btnModal = document.getElementById("btn_modal")
+	btnModal.addEventListener("click", e => { modalOn() })
+
+	const closeBtn = modal.querySelector(" .close-area")
+	closeBtn.addEventListener("click", e => { modalOff() })
+
+	modal.addEventListener("click", e => {
+		const evTarget = e.target
+		if (evTarget.classList.contains("modal-overlay")) {
+			modalOff()
+		}
+	})
+	window.addEventListener("keyup", e => {
+		if (isModalOn() && e.key === "Escape") {
+			modalOff()
+		}
+	})
+}
