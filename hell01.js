276eba095f0dcdf8386089354eda7cde52dd086c
deasaungkim <heypitcher@hanmail.net>
2022-10-19 15:03:15 +0900

    BMIFunc comment reduce

--- hell01.js
+++ hell01.js
@@ -17,20 +17,6 @@ function selectAll(selectAll) {
 // }
 
 // BMI
-/*키의값 const height= document.getElementById('InputHeight').value */
-/*몸무게의 값 const weight= document.getElementById('InputWeight').value */
-
-// var bt = document.getElementById("bmibt");
-// bt.addEventListener("submit", BMIFunc);
-// const height = document.getElementById('InputHeight').value
-// const weight = document.getElementById('InputWeight').value
-// var BMI = weight / (height ** 2);
-// var bt=document.getElementById("bmibyt").addEventListener('submit', BMIFunc)
-// console.log()
-// window.onload = () => {
-// var bt = document.getElementById('bmibtn-submit')
-// bt.addEventListener("click", BMIFunc)
-
 function BMIFunc() {
 
 	const height = document.getElementById('InputHeight').value
@@ -40,26 +26,18 @@ function BMIFunc() {
 	var BMI = (weight / ((height ** 2) / 10000)).toFixed(2)
 	console.log(BMI)
 	if (BMI <= 18.5) {
-		// document.write('저체중').getElementById('BMIResult').value
-		// result.innerText = '저체중'
 		var bmiclass = '저체중'
 	} else if ((BMI > 18.5) && (BMI < 22.9)) {
-		// result.innerText = '정상'
 		var bmiclass = '정상'
 	} else if ((BMI > 23.0) && (BMI < 24.9)) {
-		// result.innerText = '과체중'
 		var bmiclass = '과체중'
 	} else {
-		// result.innerText = '비만'
 		var bmiclass = '비만'
 	}
 
-	// console.log(bmiclass);
 	result.innerText = bmiclass;
 	event.preventDefault();
-	// document.getElementById('BMIResult').innerText = bmiclass;
 };
-// }; return result.innerText = bmiclass;
 // 왜 input을 div로 바꾸니까 작동되지?
 
 // .아침에 뭐 먹었어요 ?
